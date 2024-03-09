33146aec71242f52f63f2b9863fbfa7d7a189b7e
d-ben-b <bend06b@gmail.com>
2024-03-09 13:20:38 +0800

    add slide

--- HW_UI/HW1.js
+++ HW_UI/HW1.js
@@ -1,6 +1,7 @@
 
 var _isopen = new Boolean(false);
 _isopen = false;
+let slideIndex = 1;
 
 $('.sign_in').click(function(){
     $('.sign_in_panel').transition('scale');
@@ -76,3 +77,81 @@ window.addEventListener("mousemove", (e) => {
   });
 });
 /**eye move**/ 
+
+/**slide**/
+// window.onload = function () {
+//   let slideIndex = 1;
+//   showSlide(slideIndex);
+
+//   let prev = document.getElementById("prev");
+//   prev.addEventListener("click", divideSlides, false);
+
+//   let next = document.getElementById("next");
+//   next.addEventListener("click", plusSlides, false);
+
+//   const selectdot = document.querySelectorAll(".dot");
+//   for (let i = 0; i < selectdot.length; i++) {
+//     selectdot[i].addEventListener("click", function (e) {
+//       showSlide((slideIndex = i + 1));
+//     });
+//   }
+
+//   function plusSlides() {
+//     showSlide((slideIndex += 1));
+//   }
+
+//   function divideSlides() {
+//     showSlide((slideIndex -= 1));
+//   }
+
+//   function showSlide(num) {
+//     let slides = document.getElementsByClassName("slide__item");
+//     let dots = document.getElementsByClassName("dot");
+//     if (num > slides.length) {
+//       slideIndex = 1;
+//     }
+
+//     if (num < 1) {
+//       slideIndex = slides.length;
+//     }
+//     for (let i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (let i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace("active", "");
+//     }
+
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += "active";
+//   }
+// };
+
+$('.slide__arraw').click(function(){
+  console.log("HI")
+  showSlide(slideIndex);
+
+  function showSlide(num){
+    let slides = document.getElementsByClassName("Image");
+    slides[num].style.display = "block";
+    let dots = document.getElementsByClassName("dot");
+    dots[num].style.background = "rgba(255, 255, 255, 0.9)";
+
+    if(slideIndex < 5){
+      slideIndex++;
+    }
+    else{
+      slideIndex=1
+    }
+
+    if(num==1){
+      slides[5].style.display = "none";
+      dots[5].style.background = rgba(255, 255, 255, 0.9);
+    }
+    else{
+      slides[num-1].style.display = "none";
+      $('dots[num-1]').transition('scale');
+    }
+
+    console.log("slideIndex=" + slideIndex)
+  } 
+})
